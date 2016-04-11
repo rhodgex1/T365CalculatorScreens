611fbc3832ac51926b422aa905956395a1bd67ae
Richard Hodge <rhodge@x1labs.com>
2016-04-11 15:43:56 -0500

    Added base project

---
+++ T365CalculatorScreens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T365CalculatorScreens
+//
+//  Created by Richard Hodge on 4/11/16.
+//  Copyright © 2016 Xome Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
