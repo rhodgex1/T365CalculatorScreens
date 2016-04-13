ae06f189b9a9d13a4f4ba2e70d02aff6b93060c2
DevarshiK <devarshi.k@techendeavour.com>
2016-04-13 11:49:52 +0530

    Binded outlets to respective components. Removed sample data from UI. Updated keyboard type for textFields, wherever needed. Updated selection of each static cell to 'No Selection'

--- T365CalculatorScreens/NetSheetReports/PreparedByController.swift
+++ T365CalculatorScreens/NetSheetReports/PreparedByController.swift
@@ -2,6 +2,17 @@ import UIKit
 
 // TableViewController mapped to a TableView with static cells
 class PreparedByController: UITableViewController {
+    // Outlets declarations
+    @IBOutlet weak var includePreparedBySwitch: UISwitch!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var companyField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var zipCodeField: UITextField!
+    @IBOutlet weak var phoneNumberField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
