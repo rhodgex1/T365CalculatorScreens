8f220792e34579e5e68ff70cdd1b273a32f9e173
DevarshiK <devarshi.k@techendeavour.com>
2016-04-19 16:47:39 +0530

    Completed Escrow screen

---
+++ T365CalculatorScreens/SellerNetSheet/EscrowController.swift
@@ -0,0 +1,25 @@
+//
+//  EscrowController.swift
+//  T365CalculatorScreens
+//
+//  Created by Devarshi Kulshreshtha on 4/19/16.
+//  Copyright © 2016 Xome Labs. All rights reserved.
+//
+
+import UIKit
+
+class EscrowController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Escrow"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
