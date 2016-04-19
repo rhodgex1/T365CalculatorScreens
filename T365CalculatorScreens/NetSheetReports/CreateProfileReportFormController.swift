678f625b7cfe6d9a78248cc7c08c403edb2ee555
DevarshiK <devarshi.k@techendeavour.com>
2016-04-19 18:25:32 +0530

    Completed basic layout of 'Financing' screen

--- T365CalculatorScreens/NetSheetReports/CreateProfileReportFormController.swift
+++ T365CalculatorScreens/NetSheetReports/CreateProfileReportFormController.swift
@@ -1,5 +1,14 @@
 import UIKit
 
+enum ReportContentSelectionButtonTag : Int {
+    case PropertyDetail = 201
+    case MapOfProperty = 202
+    case PropertyValuationModel = 203
+    case SalesComparableSummary = 204
+    case SalesComparableDetailed = 205
+    case SalesComparableMap = 206
+    case ParcelMap = 207
+}
 class CreateProfileReportFormController: UITableViewController {
 
     override func viewDidLoad() {
@@ -18,6 +27,33 @@ extension CreateProfileReportFormController {
     
     @IBAction func selectContent(sender: UIButton) {
         sender.selected = !sender.selected
+        
+        // perform action based on button tag
+        if let reportContent = ReportContentSelectionButtonTag(rawValue:sender.tag) {
+            switch reportContent {
+                case .PropertyDetail:
+                    // perform PropertyDetail related action
+                    print("PropertyDetail selected");
+                case .MapOfProperty:
+                    // perform MapOfProperty related action
+                    print("MapOfProperty selected");
+                case .PropertyValuationModel:
+                    // perform PropertyValuationModel related action
+                    print("PropertyValuationModel selected");
+                case .SalesComparableSummary:
+                    // perform SalesComparableSummary related action
+                    print("SalesComparableSummary selected");
+                case .SalesComparableDetailed:
+                    // perform SalesComparableDetailed related action
+                    print("SalesComparableDetailed selected");
+                case .SalesComparableMap:
+                    // perform SalesComparableMap related action
+                    print("SalesComparableMap selected");
+                case .ParcelMap:
+                    // perform ParcelMap related action
+                    print("ParcelMap selected");
+            }
+        }
     }
     
 }
---
+++ T365CalculatorScreens/SellerNetSheet/FinancingController.swift
@@ -0,0 +1,25 @@
+//
+//  FinancingController.swift
+//  T365CalculatorScreens
+//
+//  Created by Devarshi Kulshreshtha on 4/19/16.
+//  Copyright © 2016 Xome Labs. All rights reserved.
+//
+
+import UIKit
+
+class FinancingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Financing"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
