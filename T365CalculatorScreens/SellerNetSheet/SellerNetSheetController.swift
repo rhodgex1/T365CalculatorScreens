7b3f8dff71f99842e5f8bc0aa2a2456012c6ded5
DevarshiK <devarshi.k@techendeavour.com>
2016-04-26 18:35:09 +0530

    Implemented reset button display/hide functionality. Integrated reset action.

--- T365CalculatorScreens/SellerNetSheet/SellerNetSheetController.swift
+++ T365CalculatorScreens/SellerNetSheet/SellerNetSheetController.swift
@@ -11,6 +11,20 @@ class SellerNetSheetController: SellerNetSheetWithContainersBaseController {
         
         // Note: header controller (netProceedsController) is being set in its super class
         sellerNewNetSheetFormController = bodyContentController as! SellerNewNetSheetForm
+        sellerNewNetSheetFormController.addResetButtonHandler = { [weak self] in
+            guard let this = self else {
+                return
+            }
+           this.addResetBarButtonItem()
+        }
+        sellerNewNetSheetFormController.removeResetButtonHandler = { [weak self] in
+            guard let this = self else {
+                return
+            }
+            this.removeResetBarButtonItem()
+        }
+        
+        displayRightBarButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,15 +34,53 @@ class SellerNetSheetController: SellerNetSheetWithContainersBaseController {
 
 }
 
+extension SellerNetSheetController {
+    private func addResetBarButtonItem() {
+        if navigationItem.rightBarButtonItems?.count == 1 {
+            navigationItem.rightBarButtonItems?.insert(resetBarButtonItem(), atIndex: 1)
+        }
+    }
+    
+    private func removeResetBarButtonItem() {
+        if navigationItem.rightBarButtonItems?.count == 2 {
+            navigationItem.rightBarButtonItems?.removeLast()
+        }
+    }
+    
+    private func displayRightBarButtonItem() {
+        navigationItem.rightBarButtonItems = [addMoreBarButtonItem()]
+    }
+    
+    private func addMoreBarButtonItem() -> UIBarButtonItem {
+        let moreButton = UIButton()
+        moreButton.setImage(UIImage(named: "actionIcon"), forState: .Normal)
+        moreButton.frame = CGRectMake(0, 0, 30, 30)
+        moreButton.addTarget(self, action: #selector(SellerNetSheetController.moreAction(_:)), forControlEvents: .TouchUpInside)
+        let moreBarItem = UIBarButtonItem()
+        moreBarItem.customView = moreButton
+        
+        return moreBarItem
+    }
+    
+    private func resetBarButtonItem() -> UIBarButtonItem{
+        let resetButton = UIButton()
+        resetButton.setImage(UIImage(named: "resetIcon"), forState: .Normal)
+        resetButton.frame = CGRectMake(0, 0, 30, 30)
+        resetButton.addTarget(sellerNewNetSheetFormController, action: #selector(SellerNewNetSheetForm.resetAction(_:)), forControlEvents: .TouchUpInside)
+        let resetBarItem = UIBarButtonItem()
+        resetBarItem.customView = resetButton
+        
+        return resetBarItem
+    }
+    
+   
+    func moreAction(sender: UIButton) {
+        // more action
+    }
+}
+
 //MARK: User actions
 extension SellerNetSheetController {
-    
-    @IBAction func moreAction(sender: AnyObject) {
-    }
-    
-    //TODO: remove all data values shown in form
-    @IBAction func resetAction(sender: AnyObject) {
-    }
-
+ 
     
 }
