f2cc94a97e01c53af09706030c1c09d59ec6e162
DevarshiK <devarshi.k@techendeavour.com>
2016-05-02 18:05:02 +0530

    Implemented 'Profile Credits' screen

---
+++ T365CalculatorScreens/Settings/ProfileCreditsForm.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileCreditsForm.swift
+//  T365CalculatorScreens
+//
+//  Created by Devarshi Kulshreshtha on 5/2/16.
+//  Copyright © 2016 Xome Labs. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCreditsForm: UITableViewController {
+
+    // declaring outlets
+    @IBOutlet weak var totalDailyCredits: UILabel!
+    @IBOutlet weak var totalMonthlyCredits: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
