8e790f2d2794045e04bb79332b43d38f9b3e7fad
DevarshiK <devarshi.k@techendeavour.com>
2016-05-03 13:30:30 +0530

    Integrated UIImagePickerController

--- T365CalculatorScreens/Settings/Feedback/FeedbackForm.swift
+++ T365CalculatorScreens/Settings/Feedback/FeedbackForm.swift
@@ -15,6 +15,7 @@ class FeedbackForm: UITableViewController {
     @IBOutlet weak var osVersion: UILabel!
     @IBOutlet weak var appVersion: UILabel!
 
+    private var imagePickerController: UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +25,54 @@ class FeedbackForm: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+//MARK:- TableView delegate methods
+extension FeedbackForm {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cellReuseIdentifier = super.tableView(tableView, cellForRowAtIndexPath: indexPath).reuseIdentifier where cellReuseIdentifier == "ScreehshotsCell"{
+            // Showing UIImagePickerController
+            if imagePickerController == nil {
+                imagePickerController = UIImagePickerController()
+            }
+            
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+                imagePickerController!.delegate = self
+                imagePickerController!.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+                
+                presentViewController(imagePickerController!, animated: true, completion: nil)
+            }
+            
+        }
+    }
+}
+
+//MARK:- UINavigationControllerDelegate implementation
+extension FeedbackForm : UINavigationControllerDelegate {
+    // configure shown view controller
+    func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool)
+    {
+        viewController.navigationItem.title = "Screenshots"
+        
+    }
+}
+
+//MARK:- UIImagePickerControllerDelegate implementation
+extension FeedbackForm : UIImagePickerControllerDelegate{
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let _ = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //TODO: do somethign with picked image
+
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+
+    
 }
