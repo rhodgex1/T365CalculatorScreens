e0c6c62ce2f4f77c9622bec8359085276be0ae02
DevarshiK <devarshi.k@techendeavour.com>
2016-05-09 18:41:10 +0530

    User Profile Options screen: completed basic layout and added placeholder actions. Started adding UI elements to Account rep > my profile > edit screen

---
+++ T365CalculatorScreens/Profile/MyProfile/AddressTitleCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddresTitleCell.swift
+//  T365CalculatorScreens
+//
+//  Created by Devarshi Kulshreshtha on 5/9/16.
+//  Copyright © 2016 Xome Labs. All rights reserved.
+//
+
+import UIKit
+
+class AddresTitleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
